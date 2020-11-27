5cc108ee5bb7be96bdae44a0eb02452c2fd23529
ProgerGagik <gagikgrigoryan2005@mail.ru>
2020-11-27 12:33:33 +0300

    main

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 import sys
-import math
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+import random
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("calc.ui", self)
-        self.btn1.clicked.connect(self.checker)
-        self.btn2.clicked.connect(self.checker)
-        self.btn3.clicked.connect(self.checker)
-        self.btn4.clicked.connect(self.checker)
-        self.btn5.clicked.connect(self.checker)
-        self.btn6.clicked.connect(self.checker)
-        self.btn7.clicked.connect(self.checker)
-        self.btn8.clicked.connect(self.checker)
-        self.btn9.clicked.connect(self.checker)
-        self.btn0.clicked.connect(self.checker)
-        self.btn_clear.clicked.connect(self.clr)
-        self.btn_div.clicked.connect(self.checker)
-        self.btn_mult.clicked.connect(self.checker)
-        self.btn_minus.clicked.connect(self.checker)
-        self.btn_plus.clicked.connect(self.checker)
-        self.btn_pow.clicked.connect(self.checker)
-        self.btn_sqrt.clicked.connect(self.checker)
-        self.btn_dot.clicked.connect(self.checker)
-        self.btn_fact.clicked.connect(self.checker)
-        self.btn_eq.clicked.connect(self.checker)
-        self.nmb = ""
-        self.itoge = ""
+        uic.loadUi("UI.ui", self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
-    def checker(self):
 
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(216, 216, 39))
+        qp.drawEllipse(random.randint(0, 300), random.randint(0, 300), random.randint(20, 100), random.randint(20, 100))
 
 
 if __name__ == '__main__':
